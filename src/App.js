a0daa6d29e36d2a011c46096441611b6bf516669
Anthony Robinson <robinsonamk@gmail.com>
2020-07-16 19:22:33 -0700

    created header with linked pages using react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="Header-title"> SK1 Solutions </h1>
+          <div className="Menu-items">
+            <ul>
+              <li>
+                <Link to="/services">Services</Link>
+            </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+              <li>
+                <Link to="/about">About</Link>
+            </li>
+            </ul>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path="/services">
+            <Services />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
+}
+
+function Services() {
+  return (
+    <div>
+      <h2>Services</h2>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
 }
 
 export default App;
